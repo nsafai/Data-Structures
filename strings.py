2964bbd0d6a864466fbdcd86c264770570f89f9d
Nicolai Safai <nicolaisafai@gmail.com>
2019-04-13 20:07:55 -0700

    better comment

--- strings.py
+++ strings.py
@@ -13,7 +13,7 @@ def find_index(text, pattern):
     elif len(pattern) > len(text): # pattern is too long to fit in text
         return None
 
-    # for every letter in text while there's enough text left to fit pattern
+    # for every letter in text while there's enough characters left to fit pattern
     for text_idx in range(len(text) - len(pattern) + 1):
         for pattern_idx, pattern_letter in enumerate(pattern): # look ahead for length of pattern to determine if there's a match
             if text[text_idx + pattern_idx] == pattern_letter: # if letters match
