558c5678e76ba399701b5a0a2700ed380969b52b
Nicolai Safai <nicolaisafai@gmail.com>
2019-05-08 13:45:19 -0700

    first functional version that parses txt files

--- call_routing.py
+++ call_routing.py
@@ -1,34 +1,43 @@
-from binarytree import BinarySearchTree, BinaryTreeNode
-from hashtable import HashTable
+# partially pair programmed with github.com/alishalabi
+import random # to generate random numbers
+import re # for regular expressions
+import sys # for command line args
+from binarytree import BinarySearchTree, BinaryTreeNode # to store prefixes
 
 class CallRouter(object):
-    # pair programmed with github.com/alishalabi
-    def __init__(self, phone_numbers_txt_files, route_prices_txt_files):
+    def __init__(self, phone_numbers_path, route_prices_path):
           # Turn txt files (data sources) into python objects
-          self.phone_numbers = self.parse_phone_numbers(phone_numbers_txt_files)
+          self.phone_numbers = self.parse_phone_numbers(phone_numbers_path)
           # Binary tree of prefixes. Using a BST allows us to find longest prefix as fast as possible
           self.prefixes = BinarySearchTree()
           # Contains best price per unique prefix. Using a dict allows quick fetch AND update
           self.prices = {}
           # Parse routes which populates self.prefixes and self.prices
-          self.parse_routes(route_prices_txt_files)
-          # Look up costs
-          self.save_routing_costs(self.phone_numbers)
-          
-    def parse_phone_numbers(self, phone_numbers_txt_files):
+          self.parse_routes(route_prices_path)
+
+    def turn_txt_file_into_array(self, path_to_file):
+        """Turns txt file into list without '\n' or '+' characters"""
+        file = open(path_to_file, 'r')
+        file_content = file.read() # string representation of .txt file
+        file.close()
+        # (1) Remove '+', turn into array and (2) remove last (empty) item
+        array = re.sub(r'\+', "", file_content).split('\n')
+        array.pop() # remove last item of array which is empty
+        return array
+
+    def parse_phone_numbers(self, phone_numbers_path):
         """Turns txt file into list of phone numbers without the +"""
-        phone_numbers = []
-        for number in phone_numbers_txt_files:
-            phone_numbers.append(number)
-        return phone_numbers
+        return self.turn_txt_file_into_array(phone_numbers_path)
     
-    def parse_routes(self, route_prices_txt_files):
-        """ Goes through route_prices_txt_files and creates a binary tree """
-        # For every line of file
-        for line in route_prices_txt_files:
-            route = line.split(',')
-            prefix = route[0]
-            price = route[1]
+    def parse_routes(self, route_prices_path):
+        """ Goes through route_prices_path and creates a binary tree """
+        # Parse .txt file
+        routes = self.turn_txt_file_into_array(route_prices_path)
+
+        # For every route
+        for route in routes:
+            # get prefix and price (separated by a comma)
+            prefix, price = route.split(',')
 
             if self.prefixes.contains(prefix):
                 # Check if price is cheaper
@@ -44,7 +53,6 @@ class CallRouter(object):
         last_digit_idx = len(str(phone_number)) - 1
         # Search for full phone number inside prefix, then remove one digit at a time
         while last_digit_idx > 0:
-            print(last_digit_idx)
             substring = phone_number[0:last_digit_idx]
             if self.prefixes.contains(substring):
                 return self.prices[substring]
@@ -58,33 +66,18 @@ class CallRouter(object):
         result = []
 
         for number in phone_numbers:
-            print(number)
             cost = self.get_routing_cost(number)
             line = "{},{}".format(number, cost)
             result.append(line)
             # save number and cost to text file
-        print(result)
         return result
 
 def test_call_router():
-    phone_numbers = ["86153023841"]
-    routes = [
-      "449275049,0.49",
-      "86153,0.84",
-      "8130,0.68",
-      "4928843955,0.40",
-      "449187847,0.48",
-      "8197753,0.75",
-      "449916707,0.58",
-      "64655676,0.40",
-      "8615302,1.54",
-      "34924199,0.39",
-      "1941613,0.05",
-      "61312545,0.43",
-      "8615302,1.84",
-      "8615302,1.04"
-    ]
-    call_router = CallRouter(phone_numbers, routes)
+    phone_numbers_path = 'call-routing-files/phone-numbers-1000.txt'
+    route_prices_path = 'call-routing-files/route-costs-35000.txt'
+    call_router = CallRouter(phone_numbers_path, route_prices_path)
+    # Look up costs
+    print(call_router.save_routing_costs(call_router.phone_numbers))
 
 if __name__ == '__main__':
     test_call_router()
